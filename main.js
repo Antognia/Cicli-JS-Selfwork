8063e92969a0b159d313d6dad58670c23c8e9475
Veronica <sandrinveronica@gmail.com>
2024-06-10 18:08:08 +0200

    Conteggio ciclo2

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 // Ciclo Tabellina del 2
 
 
-for (let num = 2; num <= 20; num++) {
-    if(num % 2 == 0){
-        console.log(num);
-    }
+// for (let num = 2; num <= 20; num++) {
+//     if(num % 2 == 0){
+//         console.log(num);
+//     }
+// }
+
+
+
+// Ciclo - 2
+// Contare da 1 a 20 (solo pari)
+let pari = 0;
+let dispari = 0;
+
+for (let i = 1; i < 20; i++) {
+    if (pari % 2 == 0) continue;
+    } 
     
-}
+
+    console.log(`Pari ${pari} e dispari ${dispari}`);
+
+
+
+
+
